a922ea991c2dfd609004428488a2b6ac59dd36f6
Dilo Wagner <diegowagner4@gmail.com>
2015-07-31 20:26:17 -0300

    Commit inicial

---
+++ src/main/java/br/com/discovery/spring/App.java
@@ -0,0 +1,13 @@
+package br.com.discovery.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
