7a1e7625fdfe4012f0b14fd519921b9d35a496a7
Dilo Wagner <diegowagner4@gmail.com>
2015-07-31 23:28:44 -0300

    Enviando pagina de Login

--- src/main/java/br/com/discovery/spring/Application.java
+++ src/main/java/br/com/discovery/spring/Application.java
@@ -2,15 +2,44 @@ package br.com.discovery.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+import br.com.discovery.spring.security.CsrfHeaderFilter;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
-public class Application 
+public class Application
 {
-    public static void main( String[] args )
-    {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic().and().authorizeRequests()
+					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll().anyRequest()
+					.authenticated().and().csrf()
+					.csrfTokenRepository(csrfTokenRepository()).and()
+					.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
+		}
+
+		private CsrfTokenRepository csrfTokenRepository() {
+			HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+			repository.setHeaderName("X-XSRF-TOKEN");
+			return repository;
+		}
+	}
 }
---
+++ src/main/java/br/com/discovery/spring/controller/UserController.java
@@ -0,0 +1,16 @@
+package br.com.discovery.spring.controller;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController
+{
+	@RequestMapping("/user")
+	public Principal user(Principal user)
+	{
+		return user;
+	}
+}
